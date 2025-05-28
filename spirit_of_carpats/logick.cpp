77d507b9d940560032d0f345d97440c64ec6eb8b
matviu08 <149872744+matviu08@users.noreply.github.com>
2025-05-28 18:06:35 +0300

    Add project files.

---
+++ spirit_of_carpats/logick.cpp
@@ -0,0 +1,2 @@
+#include "logick.h"
+
---
+++ spirit_of_carpats/spirit_of_carpats.cpp
@@ -0,0 +1,36 @@
+﻿#pragma comment (lib, "winmm.lib")
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+#include <conio.h>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+#include "menu.h"
+#include "logick.h"
+#include "location.h"
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+}
