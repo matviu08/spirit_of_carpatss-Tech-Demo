2e7c3e1f7b43a2d8564f7b0fbf05792ed7a8975e
BobrickCS22 <132747159+BobrickCS22@users.noreply.github.com>
2025-06-01 08:58:14 +0300

    пергий левл(без газет)

--- spirit_of_carpats/location.cpp
+++ spirit_of_carpats/location.cpp
@@ -2,11 +2,16 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace sf;
 using namespace std;
 
-int location_1_home(RenderWindow& window) {
+int generate_landscape(RenderWindow& window) {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    // --- Загрузка текстур ---
     Texture backgroundTexture;
     if (!backgroundTexture.loadFromFile("assets/img/Game_BackGround.png")) {
         cerr << "Error loading background\n";
@@ -14,70 +19,56 @@ int location_1_home(RenderWindow& window) {
     }
     Sprite background(backgroundTexture);
 
-    Texture home_element;
-    if (!home_element.loadFromFile("assets/img/home_element.png")) {
-        cerr << "Error loading home element\n";
+    Texture grassTexture;
+    if (!grassTexture.loadFromFile("assets/img/grass.png")) {
+        cerr << "Error loading grass texture\n";
         return -1;
     }
 
-    vector<Sprite> home_elements;
-    for (int i = 0; i < 4; ++i) {
-        Sprite home_element_sprite(home_element);
-        home_element_sprite.setPosition(Vector2f(100.f + i * 150.f, 400.f));
-        home_elements.push_back(home_element_sprite);
+    Texture treeTexture;
+    if (!treeTexture.loadFromFile("assets/img/tree_forest.png")) {
+        cerr << "Error loading tree texture\n";
+        return -1;
     }
 
+    // --- Генерация травы ---
+    vector<Sprite> grassTiles;
+    int tileWidth = grassTexture.getSize().x;
+    int tileHeight = grassTexture.getSize().y;
+
+    int rows = 3;
+    int cols = window.getSize().x / tileWidth + 1;
+
+    for (int y = 0; y < rows; ++y) {
+        for (int x = 0; x < cols; ++x) {
+            Sprite tile(grassTexture);
+            tile.setPosition(sf::Vector2f(static_cast<float>(x * tileWidth), static_cast<float>(window.getSize().y - (rows - y) * tileHeight)));
+            grassTiles.push_back(tile);
+        }
+    }
+
+    // --- Генерация деревьев случайно ---
+    vector<Sprite> trees;
+    for (int i = 0; i < 10; ++i) {
+        Sprite tree(treeTexture);
+        float x = static_cast<float>(rand() % (window.getSize().x - 100));
+        float y = static_cast<float>(rand() % 200 + 250);
+        tree.setPosition(sf::Vector2f(x, y));
+        trees.push_back(tree);
+    }
+
+
+
+    // --- Отрисовка ---
     window.clear();
     window.draw(background);
-    for (const auto& e : home_elements) {
-        window.draw(e);
-    }
+    for (const auto& g : grassTiles) window.draw(g);
+    for (const auto& t : trees) window.draw(t);
     window.display();
 
     return 0;
-}
 
-//int location_2_forest(RenderWindow& window) {
-//    Texture forestBackground;
-//    if (!forestBackground.loadFromFile("assets/img/Game_BackGround.png")) {
-//        cerr << "Error loading background\n";
-//        return -1;
-//    }
-//    Sprite background(forestBackground);
-//
-//    Texture treesTexture_forest;
-//    if (!treesTexture_forest.loadFromFile("assets/img/tree_forest.png")) {
-//        cerr << "Error loading tree texture\n";
-//        return -1;
-//    }
-//
-//    vector<Sprite> trees_forest;
-//    for (int i = 0; i < 5; ++i) {
-//        Sprite tree_forest(treesTexture_forest);
-//        tree_forest.setPosition(Vector2f(100.f + i * 150.f, 300.f));
-//        trees_forest.push_back(tree_forest);
-//    }
-//
-//    Texture ground_texture;
-//    if (!ground_texture.loadFromFile("assets/img/grass.png")) {
-//        cerr << "Error loading grass texture\n";
-//        return -1;
-//    }
-//
-//    Sprite ground(ground_texture);
-//    ground.setPosition(0, 550);
-//    ground.setScale(
-//        static_cast<float>(window.getSize().x) / ground_texture.getSize().x,
-//        1.0f
-//    );
-//
-//    window.clear();
-//    window.draw(background);
-//    for (const auto& t : trees_forest) {
-//        window.draw(t);
-//    }
-//    window.draw(ground);
-//    window.display();
-//
-//    return 0;
-//}
+    //generate_landscape(window); -для виклику сцени через меню
+
+
+}
