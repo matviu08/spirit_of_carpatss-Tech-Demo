a4d32eaeb44b13632ae64c3fbd7111516510a122
forking2 <104146383+forking2@users.noreply.github.com>
2025-06-27 19:03:10 +0300

    asf

--- spirit_of_carpats/chapters.cpp
+++ spirit_of_carpats/chapters.cpp
@@ -4,32 +4,32 @@
 #include <iostream>
 #include <vector>
 
-void createLevel1(sf::RenderWindow& window, sf::Sprite& backgroundSprite, const std::optional<sf::Event>& event, Menu& menu) {
-    std::cout << "Запускаємо Level 1..." << std::endl;
+void createLevel1(RenderWindow& window, Sprite& backgroundSprite, const optional<Event>& event, Menu& menu) {
+    cout << "Запускаємо Level 1..." << endl;
     Player pl;
        
-    sf::Texture newBackgroundTexture;
+    Texture newBackgroundTexture;
     if (!newBackgroundTexture.loadFromFile("assets/img/levl1_bg.png")) {
-        std::cerr << "неможливо завантажити background.jpg!" << std::endl;
+        cout << "неможливо завантажити background.jpg!" << endl;
         return;
     }
 
-    sf::Vector2u textureSize = newBackgroundTexture.getSize();
-    sf::Vector2u windowSize = window.getSize();
+    Vector2u textureSize = newBackgroundTexture.getSize();
+    Vector2u windowSize = window.getSize();
 
     float scaleX = static_cast<float>(windowSize.x) / textureSize.x;
     float scaleY = static_cast<float>(windowSize.y) / textureSize.y;
 
-    backgroundSprite.setScale(sf::Vector2f(scaleX, scaleY));
+    backgroundSprite.setScale(Vector2f(scaleX, scaleY));
     backgroundSprite.setPosition(Vector2f(0.f, 0.f));
 
-    sf::Font font;
+    Font font;
     if (!font.openFromFile("assets/fonts/alagard-12px-unicode.ttf")) {
-        cout << "Помилка: не вдалося завантажити шрифт!" << std::endl;
+        cout << "Помилка: не вдалося завантажити шрифт!" << endl;
     }
 
     extern bool tohnoEnglish;
-    sf::Text backButtonForChap(font);
+    Text backButtonForChap(font);
     if (tohnoEnglish) {
         backButtonForChap.setString("Exit the game");
     }
@@ -37,10 +37,10 @@ void createLevel1(sf::RenderWindow& window, sf::Sprite& backgroundSprite, const 
         backButtonForChap.setString(L"Вихід з гри");
     }
     backButtonForChap.setCharacterSize(50);
-    backButtonForChap.setFillColor(sf::Color::White);
-    backButtonForChap.setPosition(sf::Vector2f(20.f, 20.f));
+    backButtonForChap.setFillColor(Color::White);
+    backButtonForChap.setPosition(Vector2f(20.f, 20.f));
 
-    sf::Text chapterText(font);
+    Text chapterText(font);
     if (tohnoEnglish) {
         chapterText.setString("Chapter 1");
     }
@@ -48,27 +48,27 @@ void createLevel1(sf::RenderWindow& window, sf::Sprite& backgroundSprite, const 
         chapterText.setString(L"Чаптерс 1");
     }
     chapterText.setCharacterSize(80);
-    chapterText.setFillColor(sf::Color::White);
-    sf::Vector2f pos_1(static_cast<float>(windowSize.x) / 2 - chapterText.getLocalBounds().size.x / 2, static_cast<float>(windowSize.y) / 2 - chapterText.getLocalBounds().size.y / 2);
+    chapterText.setFillColor(Color::White);
+    Vector2f pos_1(static_cast<float>(windowSize.x) / 2 - chapterText.getLocalBounds().size.x / 2, static_cast<float>(windowSize.y) / 2 - chapterText.getLocalBounds().size.y / 2);
     chapterText.setPosition(pos_1);
 
-    sf::Vector2f textureSizeF = static_cast<sf::Vector2f>(sf::Vector2u(newBackgroundTexture.getSize()));
+    Vector2f textureSizeF = static_cast<Vector2f>(Vector2u(newBackgroundTexture.getSize()));
     int repeatCount = 5;
 
-    std::vector<sf::Sprite> tiledBackgrounds;
+    vector<Sprite> tiledBackgrounds;
     for (int i = 0; i < repeatCount; ++i) {
-        sf::Sprite tile(backgroundSprite);
-        tile.setPosition(sf::Vector2f(i * static_cast<float>(textureSize.x), 0.f));
+        Sprite tile(backgroundSprite);
+        tile.setPosition(Vector2f(i * static_cast<float>(textureSize.x), 0.f));
         tiledBackgrounds.push_back(tile);
     }
     
     while (window.isOpen()) {
-            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-            sf::Vector2f mouseWorldPos(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
-            window.clear(sf::Color::Black);
+            Vector2i mousePos = Mouse::getPosition(window);
+            Vector2f mouseWorldPos(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+            window.clear(Color::Black);
             window.draw(chapterText);
             window.display();
-            sf::sleep(sf::seconds(2));
+            sleep(seconds(2));
             for (auto& tile : tiledBackgrounds) {
                 window.draw(tile);
             }
