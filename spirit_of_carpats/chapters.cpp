8e82558ce0755e64f0f5a92970a34a137a968851
matviu08 <149872744+matviu08@users.noreply.github.com>
2025-06-27 14:20:26 +0300

    чорний екран добавив

--- spirit_of_carpats/chapters.cpp
+++ spirit_of_carpats/chapters.cpp
@@ -31,14 +31,26 @@ void createLevel1(sf::RenderWindow& window, sf::Sprite& backgroundSprite, const 
     extern bool tohnoEnglish;
     sf::Text backButtonForChap(font);
     if (tohnoEnglish) {
-        backButtonForChap.setString("Back to Chapters");
+        backButtonForChap.setString("Exit the game");
     }
     else {
-        backButtonForChap.setString(L"Назад до чаптерів");
+        backButtonForChap.setString(L"Вихід з гри");
     }
     backButtonForChap.setCharacterSize(50);
     backButtonForChap.setFillColor(sf::Color::White);
     backButtonForChap.setPosition(sf::Vector2f(20.f, 20.f));
+
+    sf::Text chapterText(font);
+    if (tohnoEnglish) {
+        chapterText.setString("Chapter 1");
+    }
+    else {
+        chapterText.setString(L"Чаптерс 1");
+    }
+    chapterText.setCharacterSize(80);
+    chapterText.setFillColor(sf::Color::White);
+    sf::Vector2f pos_1(static_cast<float>(windowSize.x) / 2 - chapterText.getLocalBounds().size.x / 2, static_cast<float>(windowSize.y) / 2);
+    chapterText.setPosition(pos_1);
 
     sf::Vector2f textureSizeF = static_cast<sf::Vector2f>(sf::Vector2u(newBackgroundTexture.getSize()));
     int repeatCount = 5;
@@ -53,12 +65,15 @@ void createLevel1(sf::RenderWindow& window, sf::Sprite& backgroundSprite, const 
     while (window.isOpen()) {
             sf::Vector2i mousePos = sf::Mouse::getPosition(window);
             sf::Vector2f mouseWorldPos(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
-            window.clear();
+            window.clear(sf::Color::Black);
+            window.draw(chapterText);
+            window.display();
+            sf::sleep(sf::seconds(2));
             for (auto& tile : tiledBackgrounds) {
                 window.draw(tile);
             }
             Generation_of_chapter1(window, backgroundSprite, backButtonForChap, pl, font, event, menu);
             window.display();
-        }
+    }
     
 }
